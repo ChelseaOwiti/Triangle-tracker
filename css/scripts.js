713e753b3e2c0d689d71bc78f55b5ac911e7cd9f
Chelsea Owiti <chelseaowiti@gmail.com>
2019-04-28 16:04:50 -0400

    variable text which will store answers added

--- css/scripts.js
+++ css/scripts.js
@@ -2,4 +2,21 @@ function myFunction(){
   var L1= parseInt(document.getElementById(L1).value);
   var L2= parseInt(document.getElementById(L2).value);
   var L3= parseInt(document.getElementById(L3).value);
+  var text;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

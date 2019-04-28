e0b86db3e4b3a22aabc54990eca789c427a9e502
Chelsea Owiti <chelseaowiti@gmail.com>
2019-04-28 16:27:59 -0400

    conditions for triangles less than 0 added

--- css/scripts.js
+++ css/scripts.js
@@ -4,7 +4,11 @@ function myFunction(){
   var L3= parseInt(document.getElementById(L3).value);
   var text;
 
-  
+  var triangleTracker = [L1, L2, L3];
+  // CONDITION FOR LENGTHS TO BE GREATHER THAN 0
+  if (L1<= 0|| L2<=0|| L3<=0){
+    text = "Not a triangle, triangles must have a value of more than o"
+  }
 
 
 

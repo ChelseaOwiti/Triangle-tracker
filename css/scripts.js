0bde42fda6d16b81c8c0c13281c306fa2476bb91
Chelsea Owiti <chelseaowiti@gmail.com>
2019-04-28 20:48:50 -0400

    conditions for an equilateral triangle added

--- css/scripts.js
+++ css/scripts.js
@@ -7,7 +7,7 @@ function myFunction(){
   var triangleTracker = [L1, L2, L3];
   // CONDITION FOR LENGTHS TO BE GREATHER THAN 0
   if (L1<= 0|| L2<=0|| L3<=0){
-    text = "Not a triangle, triangles must have a value of more than o"
+    text = " not a triangle, triangles must have a value of more than o"
   }
   // CONDITION FOR NOT A TRIANGLE
   else if(
@@ -15,15 +15,22 @@ function myFunction(){
     (L1 + L3 <= L2 && L3 + L1 <= L2) ||
     (L2 + L3 <= L1 && L3 + L2 <= L1)
   ){
-    text = "Not a triangle"
+    text = " not a triangle"
   }
   //  CONDITION FOR AN ISOSCLES TRIANGLE
   else if(
     L1===L2 || L1===L3 || L2===L1 || L2===L3 || L3===L1 
     || L3===L2 
   ){
-    text = "This is an Isosceles Triangle"
+    text = " an Isosceles triangle, two sides are equal"
   }
+  // CONDITIONS FOR AN EQUILATERAL TRIANGLE
+  else if(
+    L1===L2 && L2===L3 && L3===L1
+  ){
+    text = " an Equilateral triangle, all sides are equal"
+  }
+  
 
 
 

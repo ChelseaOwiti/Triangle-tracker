a0494a75545b60437e20684c1d60bdee60c441fa
Chelsea Owiti <chelseaowiti@gmail.com>
2019-04-28 22:27:48 -0400

    few changes made

--- css/scripts.js
+++ css/scripts.js
@@ -1,13 +1,19 @@
 function myFunction(){
-  var L1= parseInt(document.getElementById(L1).value);
-  var L2= parseInt(document.getElementById(L2).value);
-  var L3= parseInt(document.getElementById(L3).value);
-  var text;
+  var L1= parseInt(document.getElementById("L1").value);
+  var L2= parseInt(document.getElementById("L2").value);
+  var L3= parseInt(document.getElementById("L3").value);
 
-  var triangleTracker = [L1, L2, L3];
+  
+  // input added here
+  var array = [L1, L2, L3];
+  
+  // input stored here
+  var text;
+  
+
   // CONDITION FOR LENGTHS TO BE GREATHER THAN 0
   if (L1<= 0|| L2<=0|| L3<=0){
-    text = " not a triangle, triangles must have a value of more than o"
+    alert(text = " not a triangle, triangles must have a value of more than o");
   }
   // CONDITION FOR NOT A TRIANGLE
   else if(
@@ -15,42 +21,27 @@ function myFunction(){
     (L1 + L3 <= L2 && L3 + L1 <= L2) ||
     (L2 + L3 <= L1 && L3 + L2 <= L1)
   ){
-    text = " not a triangle"
+    alert(text = " not a triangle");
   }
   //  CONDITION FOR AN ISOSCLES TRIANGLE
   else if(
     L1===L2 || L1===L3 || L2===L1 || L2===L3 || L3===L1 
     || L3===L2 
   ){
-    text = " an Isosceles triangle, two sides are equal"
+    alert(text = " an Isosceles triangle, two sides are equal");
   }
   // CONDITIONS FOR AN EQUILATERAL TRIANGLE
   else if(
     L1===L2 && L2===L3 && L3===L1
   ){
-    text = " an Equilateral triangle, all sides are equal"
+    alert(text = " an Equilateral triangle, all sides are equal");
   }
   // CONDITIONS FOR A SCALENE TRIANGLE
   else if(
     L1 !==L2 && L2 !==L3 && L3 !==L1
   ){
-    text = " a Scalene triangle, no sides are equal"
+    alert(text = " a Scalene triangle, no sides are equal");
   }
-
-  alert("This is:" + "["  + Array.join() + "]" + "This is" + text);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 }
